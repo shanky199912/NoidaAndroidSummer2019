e2d23f2664f360c6ef6e4aec5c7850c84af224e3
shanky199912 <shanky199912@outlook.com>
2019-07-04 12:14:24 +0530

    Lecture -12

--- Lecture12/Okhttp+Retrofit/app/src/main/java/com/codingblocks/networkingokhttpretrofit/GithubService.kt
+++ Lecture12/Okhttp+Retrofit/app/src/main/java/com/codingblocks/networkingokhttpretrofit/GithubService.kt
@@ -4,6 +4,10 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface GithubService {
+    /**
+     * Api me se get and post krne ke liye interface
+     * end point is added in a constant
+     */
     @GET("search/users?q=%22Pulkit%20Aggarwal%22")
     fun listUsers(): Call<GIthubResponse>
 
